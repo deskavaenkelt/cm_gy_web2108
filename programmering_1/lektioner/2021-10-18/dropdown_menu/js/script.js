f132e84a8e39870b7e7e64bf390c53339bf49e6b
Lars Strömberg <stromberg.lars@gmail.com>
2021-10-18 12:18:26 +0200

    Dropdown menu

---
+++ programmering_1/lektioner/2021-10-18/dropdown_menu/js/script.js
@@ -0,0 +1,9 @@
+function openMenu() {
+    document.getElementById('dropdown')
+        .classList.toggle('show')
+}
+
+function closeMenu() {
+    document.getElementById('dropdown')
+        .classList.remove('show')
+}
