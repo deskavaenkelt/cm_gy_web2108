fdb6b18a50070d7c133380aee44fa89ed149360d
Lars Strömberg <stromberg.lars@gmail.com>
2021-10-18 15:53:24 +0200

    Komponenter JavaScript

--- programmering_1/lektioner/2021-10-18/componenter/js/script.js
+++ programmering_1/lektioner/2021-10-18/componenter/js/script.js
@@ -22,5 +22,32 @@ document.getElementById('event-listener-2')
 // Hämta data från ett fält
 function nameFieldButton() {
     let nameField = document.getElementById('name-field').value
-    console.log('nameFieldButton clicked, content off input:', nameField)
+    // console.log('nameFieldButton clicked, "value" of input:', nameField)
+
+    let helloNameField = document.getElementById('hello-name-field')
+    // console.log(helloNameField)
+    // document.getElementById('hello-name-field').innerText = 'Hej '
+    // helloNameField.innerText = `Hej ${nameField}!`
+    // helloNameField.innerText = `<strong>Hej ${nameField}!</strong>`
+    helloNameField.innerHTML = `<strong>Hej ${nameField}!</strong>`
 }
+
+function add(x, y) {
+    return x + y
+}
+
+function addNumbers() {
+    const number1 = Number(document.getElementById('num-field1').value)
+    const number2 = Number(document.getElementById('num-field2').value)
+    const sum = document.getElementById('sum-num-field')
+
+    sum.innerText = add(number1, number2)
+}
+
+const morE = 'more'
+
+function somethingMoreToLoadOnPageRender() {
+    document.getElementById('more').innerText = morE.toUpperCase()
+}
+
+somethingMoreToLoadOnPageRender()
