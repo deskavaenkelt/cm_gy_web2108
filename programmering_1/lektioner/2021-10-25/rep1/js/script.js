6316a7197b7f1d44224620147162b0aa6a925b18
Lars Strömberg <stromberg.lars@gmail.com>
2021-10-25 10:09:10 +0200

    Array + object

---
+++ programmering_1/lektioner/2021-10-25/rep1/js/script.js
@@ -0,0 +1,55 @@
+// Array
+let cars = ['Volvo', 'Polstar', 'Volkswagen', 'Audi', 'BMW']
+
+console.log(cars)
+console.log(cars[1])
+
+console.log('-----------------------------')
+for (let i = 0; i < cars.length; i++) {
+    console.log(cars[i])
+}
+
+console.log('-----------------------------')
+for (const item of cars) {
+    console.log(item)
+}
+
+console.log('-----------------------------')
+
+// Object
+let volvo = {
+    make: 'Volvo',
+    model: 'V90',
+    engine: 'B5',
+    price: 648759,
+    img:'url'
+}
+
+console.log(volvo)
+console.log(volvo.model)
+
+let bmw = {
+    make: 'BMW',
+    model: '740',
+    engine: 'V8',
+    price: 1648759
+}
+
+console.log(bmw)
+console.log(bmw.model)
+bmw.img= 'url'
+console.log(bmw)
+
+class Car {
+    constructor(make, model, engine, price, img) {
+        this.make = make
+        this.model = model
+        this.engine = engine
+        this.price = price
+        this.img = img
+    }
+}
+
+let audi = new Car('Audi', 'Q5', 'V8', 2349753)
+console.log(audi)
+console.log(audi.model)
