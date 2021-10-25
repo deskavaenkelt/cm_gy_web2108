3f5d03302baf0afa5b9e31481df344669afb2ecd
Lars Strömberg <stromberg.lars@gmail.com>
2021-10-25 11:00:49 +0200

    Object i Array

--- programmering_1/lektioner/2021-10-25/rep1/js/script.js
+++ programmering_1/lektioner/2021-10-25/rep1/js/script.js
@@ -22,7 +22,7 @@ let volvo = {
     model: 'V90',
     engine: 'B5',
     price: 648759,
-    img:'url'
+    img: 'url'
 }
 
 console.log(volvo)
@@ -37,7 +37,7 @@ let bmw = {
 
 console.log(bmw)
 console.log(bmw.model)
-bmw.img= 'url'
+bmw.img = 'url'
 console.log(bmw)
 
 class Car {
@@ -53,3 +53,56 @@ class Car {
 let audi = new Car('Audi', 'Q5', 'V8', 2349753)
 console.log(audi)
 console.log(audi.model)
+
+console.log('-----------------------------')
+
+let carsWithObjects = [volvo, bmw, audi]
+console.log(carsWithObjects)
+console.log(carsWithObjects[0].model)
+
+function findSomething(index) {
+    return index * 3
+}
+
+for (let i = 0; i < carsWithObjects.length; i++) {
+    console.log(carsWithObjects[i].model)
+    console.log(findSomething(i))
+}
+
+for (const car of carsWithObjects) {
+    console.log(car.model)
+}
+
+console.log('-----------------------------')
+
+let newCars = [
+    {
+        make: 'Volkswagen',
+        model: 'Passat',
+        engine: 'TDI',
+        price: 448759
+    },
+    {
+        make: 'Fiat',
+        model: '500',
+        engine: '1.2l',
+        price: 363259
+    },
+    {
+        make: 'Tesla',
+        model: 'Roadster',
+        engine: 'Electric',
+        price: 2986236
+    }
+]
+
+for (let i = 0; i < carsWithObjects.length; i++) {
+    const item = carsWithObjects[i]
+    console.log(item.model)
+    console.log(findSomething(i))
+}
+
+console.log(newCars)
+for (const item of newCars) {
+    console.log(item.model)
+}
