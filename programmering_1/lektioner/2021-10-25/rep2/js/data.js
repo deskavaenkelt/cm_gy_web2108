af0e1f676e80a1cfa970aa07b5bcf07ce50fb37a
Lars Strömberg <stromberg.lars@gmail.com>
2021-10-27 07:49:21 +0200

    rep2

---
+++ programmering_1/lektioner/2021-10-25/rep2/js/data.js
@@ -0,0 +1,20 @@
+let data = [
+    {
+        make: 'Volkswagen',
+        model: 'Passat',
+        engine: 'TDI',
+        price: 448759
+    },
+    {
+        make: 'Fiat',
+        model: '500',
+        engine: '1.2l',
+        price: 363259
+    },
+    {
+        make: 'Tesla',
+        model: 'Roadster',
+        engine: 'Electric',
+        price: 2986236
+    }
+]
