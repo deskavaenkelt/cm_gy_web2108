91e0cfd60dc228c0514ca536e44a61c4be09380d
Lars Strömberg <stromberg.lars@gmail.com>
2022-01-13 15:00:48 +0100

    Dagens lektion del 3

--- webbutveckling_2/lektioner/2022-01-13/frontend/src/components/main/Main.js
+++ webbutveckling_2/lektioner/2022-01-13/frontend/src/components/main/Main.js
@@ -1,9 +1,27 @@
 import './Main.css'
+import laptop from '../../utils/images/laptop.svg'
 
 export default function Main() {
     return (
-        <>
-            <h1>Main</h1>
-        </>
+        <div className='main-grid-container'>
+            <div className='item-a'>
+                <h1>Vi utbildar Sverige inom</h1>
+                <h2>IT och programmering</h2>
+            </div>
+
+            <div className='item-b'>
+                <p>Söker du kompetenta lärare till en yrkesutbildning, kodstuga eller gästföreläsning i Göteborg?</p>
+                <a href='#'>Hur bokar jag ett möte</a>
+            </div>
+
+            <div className='item-d'>
+                <button className='primary-button'>Våra kurser</button>
+                <button className='secondary-button'>Kontakta oss</button>
+            </div>
+
+            <div className='item-c'>
+                <img src={laptop} alt='Bild på laptop'/>
+            </div>
+        </div>
     )
 }
