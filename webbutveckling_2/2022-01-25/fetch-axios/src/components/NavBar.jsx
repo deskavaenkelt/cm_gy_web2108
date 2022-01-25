1c4e9fb394f8e90f6b3c754cd0a33bfcebf7460f
Lars Strömberg <stromberg.lars@gmail.com>
2022-01-25 11:42:09 +0100

    React adderat navbar

--- webbutveckling_2/2022-01-25/fetch-axios/src/components/NavBar.jsx
+++ webbutveckling_2/2022-01-25/fetch-axios/src/components/NavBar.jsx
@@ -1,10 +1,25 @@
-const NavBar = () => {
+import { Link } from 'react-router-dom'
+import RoutingPath from '../router/RoutingPath'
+import css from './NavBar.module.css'
 
+const NavBar = () => {
     return (
-        <>
-            <h1>NavBar</h1>
-            
-        </>
+        <nav>
+            <ul className={ css.navList }>
+                <li className={ css.item }>
+                    <Link className={ `${ css.links } ${ css.active }` }
+                          to={ RoutingPath.homeView }>HomeView</Link>
+                </li>
+                <li className={ css.item }>
+                    <Link className={ css.links }
+                          to={ RoutingPath.fetchView }>FetchView</Link>
+                </li>
+                <li className={ css.item }>
+                    <Link className={ css.links }
+                          to={ RoutingPath.axiosView }>AxiosView</Link>
+                </li>
+            </ul>
+        </nav>
     )
 }
 
