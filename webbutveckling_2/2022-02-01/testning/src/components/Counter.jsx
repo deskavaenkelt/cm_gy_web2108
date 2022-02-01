a2f64a630323bc090fcc80bc0351aea83d7e7d4a
Lars Strömberg <stromberg.lars@gmail.com>
2022-02-01 12:11:30 +0100

    fireEvent()

--- webbutveckling_2/2022-02-01/testning/src/components/Counter.jsx
+++ webbutveckling_2/2022-02-01/testning/src/components/Counter.jsx
@@ -1,13 +1,19 @@
+import { useState } from 'react'
+
 const Counter = () => {
+    const [inputValue, setInputValue] = useState(1)
 
     return (
         <>
             <h2 data-testid='header'>My Counter</h2>
-            <h1>counterValue</h1>
+            <h1 data-testid='counter'>0</h1>
 
-            <button>-</button>
-            <input type='number'/>
-            <button>+</button>
+            <button data-testid='subtract-btn'>-</button>
+            <input data-testid='input'
+                   type='number'
+                   value={inputValue}
+                   onChange={event => setInputValue(event.target.value)}/>
+            <button data-testid='add-btn'>+</button>
         </>
     )
 }
--- webbutveckling_2/2022-02-01/testning/src/components/__test__/Counter.test.jsx
+++ webbutveckling_2/2022-02-01/testning/src/components/__test__/Counter.test.jsx
@@ -1,20 +1,59 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import Counter from '../Counter'
 
-test('header renders with correct text 1', () => {
+let getByTestId
+
+beforeEach(() => {
     const component = render(<Counter/>)
-    const headerElement = component.getByTestId('header')
-    expect(headerElement.textContent).toBe('My Counter')
+    getByTestId = component.getByTestId
 })
 
-test('header renders with correct text 2', () => {
-    const { getByTestId } = render(<Counter/>)
-    const headerElement = getByTestId('header')
-    expect(headerElement.textContent).toBe('My Counter')
-})
+// test('header renders with correct text 1', () => {
+//     const component = render(<Counter/>)
+//     const headerElement = component.getByTestId('header')
+//     expect(headerElement.textContent).toBe('My Counter')
+// })
+//
+// test('header renders with correct text 2', () => {
+//     const { getByTestId } = render(<Counter/>)
+//     const headerElement = getByTestId('header')
+//     expect(headerElement.textContent).toBe('My Counter')
+// })
+//
+// test('header renders with correct text 3', () => {
+//     const { getByTestId } = render(<Counter/>)
+//
+//     expect(getByTestId('header').textContent).toBe('My Counter')
+// })
 
-test('header renders with correct text 3', () => {
-    const { getByTestId } = render(<Counter/>)
-
+test('header renders with correct text 4', () => {
     expect(getByTestId('header').textContent).toBe('My Counter')
+})
+
+test('counter initial value is 0', () => {
+    expect(getByTestId('counter').textContent).toBe('0')
+})
+
+test('input contains initial value 1', () => {
+    expect(getByTestId('input').value).toBe('1')
+})
+
+test('add button renders with +', () => {
+    expect(getByTestId('add-btn').textContent).toBe('+')
+})
+
+test('subtract button renders with -', () => {
+    expect(getByTestId('subtract-btn').textContent).toBe('-')
+})
+
+test('change value of input works correctly', () => {
+    const inputElement = getByTestId('input')
+    expect(inputElement.value).toBe('1')
+
+    fireEvent.change(inputElement, {
+        target: {
+            value: '5'
+        }
+    })
+    expect(inputElement.value).toBe('5')
 })
