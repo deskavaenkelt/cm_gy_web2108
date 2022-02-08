c7f9dff59b589035e0974c16495796a4a3f108a8
Lars Strömberg <stromberg.lars@gmail.com>
2022-02-08 12:44:30 +0100

    first server - CRUD operations

--- webbserverprogrammering_1/lektioner/2022-02-08/first_server/index.js
+++ webbserverprogrammering_1/lektioner/2022-02-08/first_server/index.js
@@ -4,11 +4,34 @@ const express = require('express')
 const port = 3001
 const app = express()
 
+// CRUD   = Create Read Update Delete
+// Method = POST   GET  PUT    DELETE
+
+// Read
 app.get('/', (req, res) => {
-    res.send('Välkommen till mitt API!')
+    console.log('GET Method')
+    res.send(`Välkommen till mitt API på port: ${port}!`)
 })
 
-app.listen(3001, () => {
+// Create
+app.post('/', (req, res) => {
+    console.log('POST Method')
+    res.send(`Got a POST request!`)
+})
+
+// Update
+app.put('/', (req, res) => {
+    console.log('PUT Method')
+    res.send(`Got a PUT/Update request!`)
+})
+
+// Delete
+app.delete('/', (req, res) => {
+    console.log('DELETE Method')
+    res.send(`Got a DELETE request!`)
+})
+
+app.listen(port, () => {
     // console.log('Server running on port 3001')
-    console.log('Server running on port http://localhost:3001')
+    console.log(`Server running on port http://localhost:${port}`)
 })
