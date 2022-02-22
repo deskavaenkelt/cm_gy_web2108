4bb2bd9662c23fb2d19db8acc94ef1ca4e2bce4a
Lars Strömberg <stromberg.lars@gmail.com>
2022-02-22 12:04:23 +0100

    Brutit ut User i separat controller

--- webbserverprogrammering_1/lektioner/2022-02-22/backend/src/controller/AliveController.js
+++ webbserverprogrammering_1/lektioner/2022-02-22/backend/src/controller/AliveController.js
@@ -1,5 +1,3 @@
-
-
 const alive = (req, res) => {
     // return res.status(200).send('API is Alive!')
     res.status(200).send('API is Alive!')
--- webbserverprogrammering_1/lektioner/2022-02-22/backend/src/server.js
+++ webbserverprogrammering_1/lektioner/2022-02-22/backend/src/server.js
@@ -2,86 +2,17 @@ import express from 'express'
 import Configuration from './configurations/configuration.js'
 import ApplyMiddlewares from './configurations/ApplyMiddlewares.js'
 import AliveController from './controller/AliveController.js'
-
+import UserController from './controller/UserController.js'
+import userDatabase from './data/userDatabase.js'
 
 // Initiate ExpressAPP
 const app = express()
 ApplyMiddlewares(app)
 
 // Database
-const userDatabase = [
-    {
-        name: 'Aram',
-        age: 30,
-        gender: 'Male'
-    },
-    {
-        name: 'Bengtina',
-        age: 19,
-        gender: 'Female'
-    },
-    {
-        name: 'Carin',
-        age: 29,
-        gender: 'Female'
-    },
-    {
-        name: 'Emil',
-        age: 32,
-        gender: 'Male'
-    }
-]
+
 
 // API Functions
-const userNames = () => {
-    const names = []
-    userDatabase.forEach(user => {
-        names.push({
-            name: user.name
-        })
-    })
-    return names
-}
-
-const getUserByName = (name) => {
-    let object = `Could not find "${ name }" in database`
-    userDatabase.forEach(user => {
-        if (name === user.name) {
-            object = user
-            return user
-        }
-    })
-    return object
-}
-
-const updateUserByName = (name, newName, age, gender) => {
-    let object = `Could not find "${ name }" in database`
-    userDatabase.forEach(user => {
-        if (name === user.name) {
-            user.name = newName
-            user.age = age
-            user.gender = gender
-            object = user
-            return
-        }
-    })
-    return object
-}
-
-const deleteUserByName = (name) => {
-    let text = `User with name: "${name}" `
-
-    for (let i = 0; i < userDatabase.length; i++) {
-        if (name === userDatabase[i].name) {
-            text += `was deleted from database!`
-            userDatabase.splice(i, 1)
-            return text
-        }
-    }
-
-    text += `don't exist in database!`
-    return text
-}
 
 // Endpoint + Business Logic
 // app.get('/', (req, res) => {
@@ -91,50 +22,18 @@ app.get('/', AliveController.alive)
 
 // CRUD
 // CREATE
-app.post('/user/', (req, res) => {
-    // const name = req.body.name
-    // const age = req.body.age
-    // const gender = req.body.gender
-    const { name, age, gender } = req.body
-    const newObject = {
-        name: name,
-        age: age,
-        gender: gender
-    }
-    userDatabase.push(newObject)
-    res.status(201).send(userDatabase)
-})
+app.post('/user/', UserController.creatUser)
 
 // READ
-app.get('/users', (req, res) => {
-    res.status(200).send(userDatabase)
-})
-
-app.get('/users/name', (req, res) => {
-    const responseFromDb = userNames()
-    res.status(200).send(responseFromDb)
-})
-
-app.get('/user/:name', (req, res) => {
-    const name = req.params.name
-    const responseFromDb = getUserByName(name)
-    res.status(200).send(responseFromDb)
-})
+app.get('/users', UserController.getUsers)
+app.get('/users/name', UserController.getUserNames)
+app.get('/user/:name', UserController.getUserByName)
 
 // UPDATE
-app.put('/user/', (req, res) => {
-    const { name, newName, age, gender } = req.body
-    const response = updateUserByName(name, newName, age, gender)
-
-    res.status(202).send(response)
-})
+app.put('/user/', UserController.updateUserByName)
 
 // DELETE
-app.delete('/user/:name', (req, res) => {
-    const name = req.params.name
-    const responseFromDB = deleteUserByName(name)
-    res.status(200).send(responseFromDB)
-})
+app.delete('/user/:name', UserController.deleteUserByName)
 
 // Start Server
 Configuration.connectToPort(app)
