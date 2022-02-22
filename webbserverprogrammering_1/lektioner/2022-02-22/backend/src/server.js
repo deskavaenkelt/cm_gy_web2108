ed18ddb324719b1f6f58700bf33fd9599d6294b5
Lars Strömberg <stromberg.lars@gmail.com>
2022-02-22 12:19:21 +0100

    Brutit ut routes

--- webbserverprogrammering_1/lektioner/2022-02-22/backend/src/server.js
+++ webbserverprogrammering_1/lektioner/2022-02-22/backend/src/server.js
@@ -1,31 +1,15 @@
 import express from 'express'
 import Configuration from './configurations/configuration.js'
 import ApplyMiddlewares from './configurations/ApplyMiddlewares.js'
-import AliveController from './controller/AliveController.js'
-import UserController from './controller/UserController.js'
+import AliveRoutes from './routes/AliveRoutes.js'
+import UserRoutes from './routes/UserRoutes.js'
 
 // Initiate ExpressAPP
 const app = express()
 ApplyMiddlewares(app)
 
-// Endpoint + Business Logic
-app.get('/', AliveController.alive)
-
-// CRUD
-
-// CREATE
-app.post('/user/', UserController.creatUser)
-
-// READ
-app.get('/users', UserController.getUsers)
-app.get('/users/name', UserController.getUserNames)
-app.get('/user/:name', UserController.getUserByName)
-
-// UPDATE
-app.put('/user/', UserController.updateUserByName)
-
-// DELETE
-app.delete('/user/:name', UserController.deleteUserByName)
+AliveRoutes.routes(app)
+UserRoutes.routes(app)
 
 // Start Server
 Configuration.connectToPort(app)
