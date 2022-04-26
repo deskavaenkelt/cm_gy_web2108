28205775c6095b82631cd4b8190028b19d234ee4
Lars Strömberg <stromberg.lars@gmail.com>
2022-04-26 13:53:25 +0200

    Alive

--- fullstack_ts_mern_projektarbete/lektioner/2022-04-26/server/src/server.ts
+++ fullstack_ts_mern_projektarbete/lektioner/2022-04-26/server/src/server.ts
@@ -1,12 +1,14 @@
 import express from 'express'
 import Configuration from './configurations/Configuration'
 import Middleware from './middlewares/Middleware'
+import AliveRoutes from './routes/AliveRoutes'
 import Logger from './utils/Logger'
 
 const server = express()
 Middleware.applyMiddlewares(server)
 
 // Routes
+AliveRoutes(server)
 
 Middleware.errorHandlerAndNotFound(server)
 
