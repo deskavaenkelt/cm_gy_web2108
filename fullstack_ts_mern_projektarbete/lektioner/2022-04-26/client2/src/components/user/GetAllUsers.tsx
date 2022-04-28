317e94c9214444e1012889af21153fe14cec079a
Lars Strömberg <stromberg.lars@gmail.com>
2022-04-28 15:03:21 +0200

    Get all users klar

--- fullstack_ts_mern_projektarbete/lektioner/2022-04-26/client2/src/components/user/GetAllUsers.tsx
+++ fullstack_ts_mern_projektarbete/lektioner/2022-04-26/client2/src/components/user/GetAllUsers.tsx
@@ -1,6 +1,7 @@
 import UserService from '../../utils/api/service/UserService'
 import { ReadUser } from '../../utils/interface/Users'
 import Card from './cards/Card'
+import CardList from './cards/CardList'
 import css from './Container.module.css'
 import { useState } from 'react'
 
@@ -10,7 +11,7 @@ const GetAllUsers = () => {
 	const getAllUsers = () => {
 		UserService.getAll()
 			.then(res => {
-				// setAllUsers(res.data)
+				setAllUsers(res.data)
 				console.log(res.data)
 			})
 			.catch(error => {
@@ -29,7 +30,7 @@ const GetAllUsers = () => {
 			<button onClick={ () => setAllUsers([]) }>Clear</button>
 			<button onClick={ clear }>Clear 2</button>
 			<br/>
-			<Card/>
+			<CardList users={allUsers}/>
 		</article>
 	)
 }
--- fullstack_ts_mern_projektarbete/lektioner/2022-04-26/client2/src/components/user/cards/Card.tsx
+++ fullstack_ts_mern_projektarbete/lektioner/2022-04-26/client2/src/components/user/cards/Card.tsx
@@ -1,13 +1,20 @@
 import { FC } from 'react'
 import { ReadUser } from '../../../utils/interface/Users'
+import css from './Table.module.css'
 
-const Card = () => {
+interface Props {
+	user: ReadUser
+}
+
+const Card: FC<Props> = ({user}) => {
 	
 	return (
-		<>
-			<h1>Card</h1>
-		
-		</>
+		<tr className={ css.border }>
+			<td className={ css.border }>{ user._id }</td>
+			<td className={ css.border }>{ user.name }</td>
+			<td className={ css.border }>{ user.age }</td>
+			<td className={ css.border }>{ user.gender }</td>
+		</tr>
 	)
 }
 
