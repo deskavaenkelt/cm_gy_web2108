8d0744480f233ceeb270fae352b68862bfb14835
Lars Strömberg <stromberg.lars@gmail.com>
2022-04-28 14:39:58 +0200

    Gett all users without card

--- fullstack_ts_mern_projektarbete/lektioner/2022-04-26/client2/src/components/user/GetAllUsers.tsx
+++ fullstack_ts_mern_projektarbete/lektioner/2022-04-26/client2/src/components/user/GetAllUsers.tsx
@@ -1,10 +1,35 @@
+import UserService from '../../utils/api/service/UserService'
+import { ReadUser } from '../../utils/interface/Users'
+import css from './Container.module.css'
+import { useState } from 'react'
+
 const GetAllUsers = () => {
+	const [allUsers, setAllUsers] = useState<Array<ReadUser>>([])
+	
+	const getAllUsers = () => {
+		UserService.getAll()
+			.then(res => {
+				// setAllUsers(res.data)
+				console.log(res.data)
+			})
+			.catch(error => {
+				console.log(error)
+			})
+	}
+	
+	const clear = () => {
+		setAllUsers([])
+	}
 	
 	return (
-		<>
-			<h1>GetAllUsers</h1>
+		<article className={ css.container }>
+			<h1>Get all users</h1>
+			<button onClick={ getAllUsers }>Get Users</button>
+			<button onClick={ () => setAllUsers([]) }>Clear</button>
+			<button onClick={ clear }>Clear 2</button>
+			<br/>
 			
-		</>
+		</article>
 	)
 }
 
