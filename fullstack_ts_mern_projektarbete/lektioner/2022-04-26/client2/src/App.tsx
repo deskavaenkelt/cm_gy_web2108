9419336e37614631dbeeb46a08152b15f52ea042
Lars Strömberg <stromberg.lars@gmail.com>
2022-04-28 13:55:30 +0200

    Alive

--- fullstack_ts_mern_projektarbete/lektioner/2022-04-26/client2/src/App.tsx
+++ fullstack_ts_mern_projektarbete/lektioner/2022-04-26/client2/src/App.tsx
@@ -1,10 +1,11 @@
 import css from './App.module.css'
+import Alive from './components/Alive'
 
 function App() {
 	return (
 		<>
 			<h1>React App written in Typescript with CSS modules</h1>
-			<p>Alive</p>
+			<Alive/>
 			<div className={ css.container }>
 				<div>
 					<p>Get All Users</p>
---
+++ fullstack_ts_mern_projektarbete/lektioner/2022-04-26/client2/src/components/Alive.tsx
@@ -0,0 +1,32 @@
+import AliveService from '../utils/api/service/AliveService'
+import { useEffect, useState } from 'react'
+import css from './Alive.module.css'
+
+const Alive = () => {
+	const [text, setText] = useState<string>('No connection to backend!')
+	const [connected, setConnected] = useState<boolean>(false)
+	
+	const alive = () => {
+		AliveService.apiConnectionStatus()
+			.then(response => {
+				// console.log(response.data)
+				setText(response.data)
+				setConnected(true)
+			})
+			.catch(error => {
+				console.error(error.message)
+			})
+	}
+	
+	useEffect(() => {
+		alive()
+	}, [])
+	
+	return (
+		<article>
+			<p className={ connected ? css.green : css.red }>{ text }</p>
+		</article>
+	)
+}
+
+export default Alive
